a94afc2c93d2cb9c1688573f41902dce2ed175b7
unknown <houxuyong@hotmail.com>
2018-05-06 23:04:40 +0800

    added JClientDev

---
+++ JClientDev/MainForm.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Bob (XuYong Hou) houxuyong@hotmail.com
+ * 日期: 2018/2/9
+ * 时间: 23:05
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using CefSharp.WinForms;
+
+namespace NGO.Pad.CefSharp
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private readonly ChromiumWebBrowser browser;
+
+		public MainForm()
+		{
+		
+            InitializeComponent();
+
+            Text = "CefSharp";
+            WindowState = FormWindowState.Maximized;
+
+            browser = new ChromiumWebBrowser("www.baidu.com")
+            {
+                Dock = DockStyle.Fill,
+            };
+            
+            
+            this.Controls.Add(browser);
+		}
+	}
+}
