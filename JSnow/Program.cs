83e0fd9ab353818381a70a93dbcefa452bd0f0a4
unknown <houxuyong@hotmail.com>
2018-05-15 23:40:41 +0800

    added complie condition

--- JSnow/Program.cs
+++ JSnow/Program.cs
@@ -14,9 +14,11 @@ namespace Snow
         [STAThread]
         static void Main()
         {
+        	#if (DIA_DEBUG)
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            #endif
         }
     }
 }
