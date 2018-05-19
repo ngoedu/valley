58267fc05ac1eab136b7c5cc52f690c5980ccd53
unknown <houxuyong@hotmail.com>
2018-05-20 01:15:52 +0800

    Control video proj.

---
+++ Control.Video/JVideo.Designer.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Bob (XuYong Hou) houxuyong@hotmail.com
+ * Date: 2018/5/20
+ * Time: 0:52
+ * 
+ * 
+ */
+namespace Control.Video
+{
+	partial class JVideo
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		
+		/// <summary>
+		/// Disposes resources used by the control.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(150, 150);
+			this.webBrowser1.TabIndex = 0;
+			// 
+			// JVideo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.webBrowser1);
+			this.Name = "JVideo";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Control.Video/JVideo.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Bob (XuYong Hou) houxuyong@hotmail.com
+ * Date: 2018/5/20
+ * Time: 0:52
+ * 
+ * 
+ */
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Control.Video
+{
+	/// <summary>
+	/// Description of JVideo.
+	/// </summary>
+	public partial class JVideo : UserControl
+	{
+		public JVideo()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			this.webBrowser1.AllowNavigation = true;
+		}
+		
+		public void NavigatgeTo(string Url) {
+			this.webBrowser1.DocumentText="";
+			this.webBrowser1.Navigate(Url);//"http://localhost/s1web/iqiyi.html");
+		}
+		
+		public void LoadHtml(string html) {
+		
+			//string theHTML = @"<html><head></head><body><p1>The paragraph</p1><ul><li>1</li><li>2</li><ul></body></html>";
+			//string iqiyi = @"<embed src='http://player.video.qiyi.com/5ef9f94c3023d2d839688acdae373314/0/0/v_19rrm6n7c4.swf-albumId=204588701-tvId=529118600-isPurchase=0-cnId=12' allowFullScreen='true' quality='high' width='480' height='350' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>";
+		
+			webBrowser1.DocumentText="";
+			webBrowser1.Document.OpenNew(true);
+			webBrowser1.Document.Write(html);
+			webBrowser1.Refresh();
+		}
+	}
+}
