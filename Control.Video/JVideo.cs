7ba47e8860a116fa26101dcf304b271ad028c988
unknown <houxuyong@hotmail.com>
2018-05-23 21:06:09 +0800

    load html

--- Control.Video/JVideo.cs
+++ Control.Video/JVideo.cs
@@ -33,11 +33,7 @@ namespace Control.Video
 			this.webBrowser1.Navigate(Url);//"http://localhost/s1web/iqiyi.html");
 		}
 		
-		public void LoadHtml(string html) {
-		
-			//string theHTML = @"<html><head></head><body><p1>The paragraph</p1><ul><li>1</li><li>2</li><ul></body></html>";
-			//string iqiyi = @"<embed src='http://player.video.qiyi.com/5ef9f94c3023d2d839688acdae373314/0/0/v_19rrm6n7c4.swf-albumId=204588701-tvId=529118600-isPurchase=0-cnId=12' allowFullScreen='true' quality='high' width='480' height='350' align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>";
-		
+		public void LoadHtml(string html) {		
 			webBrowser1.DocumentText="";
 			webBrowser1.Document.OpenNew(true);
 			webBrowser1.Document.Write(html);
