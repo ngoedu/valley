f8f5537c2722e75cbd39b786c82d12ec4cad5b39
unknown <houxuyong@hotmail.com>
2018-06-29 00:58:18 +0800

    Eide integrated

--- App.Forms/CourseView.Designer.cs
+++ App.Forms/CourseView.Designer.cs
@@ -14,6 +14,8 @@ namespace App.Forms
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private NGO.Pad.Guider.JGuider jGuider1;
+		private Control.Video.JVideo jVideo1;
 		
 		/// <summary>
 		/// Disposes resources used by the control.
@@ -26,6 +28,7 @@ namespace App.Forms
 					components.Dispose();
 				}
 			}
+			this.eide.Dispose();
 			base.Dispose(disposing);
 		}
 		
@@ -36,13 +39,33 @@ namespace App.Forms
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.jGuider1 = new NGO.Pad.Guider.JGuider();
+			this.jVideo1 = new Control.Video.JVideo();
 			this.SuspendLayout();
+			// 
+			// jGuider1
+			// 
+			this.jGuider1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.jGuider1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.jGuider1.Location = new System.Drawing.Point(0, 0);
+			this.jGuider1.Name = "jGuider1";
+			this.jGuider1.Size = new System.Drawing.Size(251, 200);
+			this.jGuider1.TabIndex = 0;
+			// 
+			// jVideo1
+			// 
+			this.jVideo1.Location = new System.Drawing.Point(0, 200);
+			this.jVideo1.Name = "jVideo1";
+			this.jVideo1.Size = new System.Drawing.Size(248, 248);
+			this.jVideo1.TabIndex = 1;
 			// 
 			// CourseView
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.Controls.Add(this.jVideo1);
+			this.Controls.Add(this.jGuider1);
 			this.Name = "CourseView";
 			this.Size = new System.Drawing.Size(707, 450);
 			this.ResumeLayout(false);
--- App.Forms/CourseView.cs
+++ App.Forms/CourseView.cs
@@ -12,6 +12,8 @@ using System.Drawing;
 using System.Windows.Forms;
 using CefSharp.WinForms;
 using CefSharp;
+using Control.Eide;
+using NGO.Train;
 
 namespace App.Forms
 {
@@ -23,7 +25,8 @@ namespace App.Forms
 		private ChromiumWebBrowser cefBrowser;
 		private NJFLib.Controls.CollapsibleSplitter splitterPanelLeft;
 		private System.Windows.Forms.Panel panelLeft;
-			
+		private JEide eide = new JEide();
+		
 		public CourseView(ChromiumWebBrowser browser)
 		{
 			//
@@ -42,7 +45,7 @@ namespace App.Forms
 			// 
 			this.splitterPanelLeft.AnimationDelay = 20;
 			this.splitterPanelLeft.AnimationStep = 20;
-			this.splitterPanelLeft.BorderStyle3D = System.Windows.Forms.Border3DStyle.Flat;
+			this.splitterPanelLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.splitterPanelLeft.ControlToHide = this.panelLeft;
 			//this.splitterPanelLeft.Dock = System.Windows.Forms.DockStyle.Right;
 			this.splitterPanelLeft.ExpandParentForm = false;
@@ -56,8 +59,8 @@ namespace App.Forms
 			// 
 			// panelLeft
 			// 
-			this.panelLeft.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.panelLeft.BackColor = System.Drawing.SystemColors.GrayText;
+			this.panelLeft.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.panelLeft.BackColor = Color.Black;
 			
 			this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
 			this.panelLeft.Location = new System.Drawing.Point(0, 80);
@@ -68,10 +71,17 @@ namespace App.Forms
 			
 			
 			this.Controls.AddRange(new System.Windows.Forms.Control[] { this.splitterPanelLeft,
-																		this.panelLeft
-																		  });
-			
+																		this.panelLeft});
 			this.panelLeft.ResumeLayout(false);
+			
+			//add Guider
+			this.panelLeft.Controls.Add(this.jGuider1);
+			
+			//add Video
+			this.panelLeft.Controls.Add(this.jVideo1);
+			
+			//add eide
+			this.Controls.Add(eide);
 		}
 		
 		private int leftPanelSize = 0;
@@ -84,23 +94,68 @@ namespace App.Forms
 			} else {
 				this.panelLeft.Width = leftPanelSize;	
 			}
-			//System.Diagnostics.Debug.WriteLine("left panel size Changed to:"+this.panelLeft.Width);
-		
 		}
 
 		
 		void LeftPanelSizeChanged(object sender, EventArgs e)
 		{
-			System.Diagnostics.Debug.WriteLine("LeftPanelSizeChanged:"+this.panelLeft.Width);
+			this.jGuider1.Top = 0;
+			this.jGuider1.Left = 0;
+			this.jGuider1.Height = 400;
+			this.jGuider1.Width = 250;
+			
+			this.jVideo1.Left = 0;
+			this.jVideo1.Top = 400 + 1;
+			this.jVideo1.Width = this.panelLeft.Width;
+			this.jVideo1.Height = this.panelLeft.Height - 400;
+			
+			this.eide.Left  = this.panelLeft.Width;
+			this.eide.Top  = 0;
+			this.eide.Width  = this.ClientSize.Width - this.panelLeft.Width;
+			this.eide.Height  = this.ClientSize.Height;
+			
+			System.Diagnostics.Debug.WriteLine("LeftPanelSizeChanged:w="+this.panelLeft.Width + ",h="+this.panelLeft.Height);
+			
+			if (this.Width == Screen.PrimaryScreen.Bounds.Width) {
+				if (!isCompomentLoaded)
+					LoadCourse("1");
+				isCompomentLoaded = true;
+			}
+				
 		}
 
-		#region ICourseView implementation
-
+		private bool isCompomentLoaded = false;
 		public void LoadCourse(string courseId)
 		{
-			throw new NotImplementedException();
+			//loading video
+			int jVideoWidth = jVideo1.Width - 20;
+			int jVideoHeight = jVideo1.Height - 20;
+			var html = "<!DOCTYPE html><html><head>	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' /><meta http-equiv='X-UA-Compatible' content='IE=Edge' />	<meta http-equiv='Content-Language' content='zh-CN'/><style type='text/css'>div { height:"+jVideoHeight+"px; width:"+jVideoWidth+"px;}body{background-color:black;}</style><script></script></head><body>	<div><iframe src='http://open.iqiyi.com/developer/player_js/coopPlayerIndex.html?vid=d52c9431203048a4986bba373d391525&tvId=1043319200&accessToken=2.f22860a2479ad60d8da7697274de9346&appKey=3955c3425820435e86d0f4cdfe56f5e7&appId=1368&height=100%&width=100%' frameborder='0' allowfullscreen='true' width='100%' height='100%'></iframe>	</div></body></html>";
+			jVideo1.LoadHtml(html);
+			
+			//bind course milestons
+			jGuider1.BindCourse(BuildCourse());
+			
+			//show eide
+			eide.LoadEide();
+			eide.EmbedIde();
+			eide.WindowsReStyle();
 		}
-
-		#endregion
+		
+		
+		private Course BuildCourse() {		
+			var course = new Course("Web编程基础A001");
+			course.AddMileStone(new Step(1, "添加一个页面","REF","Code", Course.STATUS_DEFAULT));
+			course.AddMileStone(new Step(2, "第一条文字","REF","Code", Course.STATUS_DEFAULT));
+			course.AddMileStone(new Step(3, "换行试试","REF","Code", Course.STATUS_REFER));
+			course.AddMileStone(new Step(4, "原来需要标签","REF","Code", Course.STATUS_DEFAULT));
+			course.AddMileStone(new Step(5, "样子丑陋","REF","Code", Course.STATUS_DEFAULT));
+			course.AddMileStone(new Step(6, "好多的样式","REF","Code", Course.STATUS_CODE));
+			course.AddMileStone(new Step(7, "好多的样式","REF","Code", Course.STATUS_CODE));
+			course.AddMileStone(new Step(8, "好多的样式","REF","Code", Course.STATUS_CODE));
+			course.AddMileStone(new Step(9, "好多的样式","REF","Code", Course.STATUS_DEFAULT));
+			course.AddMileStone(new Step(10, "结束","REF","Code", Course.STATUS_CODE));
+			return course;
+		}
 	}
 }
