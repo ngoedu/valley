c35f6f45c63c25b8558e8821b3afb4b7159af246
unknown <houxuyong@hotmail.com>
2018-08-31 21:31:04 +0800

    JwebBroser contorl proj refactor.

---
+++ Control.JBrowser/Browser/IBrowserJSCallback.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Bob (XuYong Hou) houxuyong@hotmail.com
+ * Date: 2018/7/24
+ * Time: 15:11
+ * 
+ * 
+ */
+using System;
+using CefSharp.WinForms;
+
+namespace Control.JBrowser
+{
+	/// <summary>
+	/// Description of IBrowserJSCallback.
+	/// </summary>
+	public interface IBrowserJSCallback
+	{
+		void SetCefBrowser(ChromiumWebBrowser cefBrowser);
+
+		string GetJSCallbackName();	
+	}
+}
