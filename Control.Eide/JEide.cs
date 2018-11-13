36aaef2f3f2ec09728d4af93f067897417d95109
unknown <houxuyong@hotmail.com>
2018-11-13 16:02:13 +0800

    eide workspace init logic added.

--- Control.Eide/JEide.cs
+++ Control.Eide/JEide.cs
@@ -64,18 +64,50 @@ namespace Control.Eide
 		}
 		
 		#region IAppEntry implementation
-		public void Init(AppRegistry reg)
+		
+		public void Init(AppRegistry reg) {
+			var projPath = (string)reg[AppRegKeys.EIDE_PROJ];
+			var cdatPath = (string)reg[AppRegKeys.EIDE_RAW_WS];
+			WorkspaceType type = Workspace.CheckWorkspaceType(projPath);
+			
+			string workspace = (string)reg[AppRegKeys.EIDE_WS];
+			bool firstTimeInit = Workspace.GetWorkspace(type).Init(cdatPath, workspace);
+		
+			//2.launch EIDE
+			this.LoadEide(false, workspace);
+			this.EmbedIde();
+			this.WindowsReStyle();
+			System.Diagnostics.Debug.WriteLine(string.Format("[EIDE] pid={0} Load + Enbed + ReStyle done.",pid));
+
+			
+			//3.import project to EIDE
+			if (firstTimeInit) {
+				IClient client = (IClient)reg[AppRegKeys.AETHER_CLIENT];
+				var projName = (string)reg[AppRegKeys.EIDE_PROJ];
+				string response = client.SendToRemoteSync(CMD_ADDPROJ+projName, ENDPOINT_ID);
+				
+				var eideResponse = EideResponse.Parse(response);
+				if (eideResponse.status.Equals(EideResponse.STATUS_OK))
+					System.Diagnostics.Debug.WriteLine("[EIDE] project "+projName+" is sucessfully added into workspace.");
+				else {
+					System.Diagnostics.Debug.WriteLine("[EIDE] add project "+projName+" to workspace is failed - " + response);
+					MessageBox.Show("[EIDE] add project "+projName+" to workspace is failed - " + response);
+				}
+			}
+		}
+		
+		public void Init1(AppRegistry reg)
 		{
 			string raw_ws = (string)reg[AppRegKeys.EIDE_RAW_WS];
 			string workspace = (string)reg[AppRegKeys.EIDE_WS];
-			bool imported = false;
+			bool firstTimeInit = false;
 			
 			//1.prepare workspace if required.			
 			if (!(new DirectoryInfo(workspace).Exists)){
 				//XTendLibs.FolderDelete.DeleteDirectory(workspace);
 				XTendLibs.FolderCopy.DirectoryCopy(raw_ws, workspace, true, null, null);
 			} else {
-				imported = true;
+				firstTimeInit = true;
 			}
 			
 			//2.launch EIDE
@@ -86,7 +118,7 @@ namespace Control.Eide
 
 	
 			//3.import project to EIDE
-			if (!imported) {
+			if (!firstTimeInit) {
 				IClient client = (IClient)reg[AppRegKeys.AETHER_CLIENT];
 				var projName = (string)reg[AppRegKeys.EIDE_PROJ];
 				string response = client.SendToRemoteSync(CMD_ADDPROJ+projName, ENDPOINT_ID);
---
+++ Control.Eide/Workspace.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Bob (XuYong Hou) houxuyong@hotmail.com
+ * Date: 2018/11/13
+ * Time: 14:17
+ * 
+ * 
+ */
+using System;
+using System.IO;
+using App.Common;
+
+namespace Control.Eide
+{
+	
+	public enum WorkspaceType {
+		Java,
+		Python,
+		SQL
+	}
+	/// <summary>
+	/// Description of IWorkspace.
+	/// </summary>
+	public abstract class Workspace
+	{
+		private static Workspace javaWorkspace = new JavaWorkspace();
+		private static Workspace pythonWorkspace = new PythonWorkspace();
+		private static Workspace sqlWorkspace = new SQLWorkspace();
+		
+		
+		public static WorkspaceType CheckWorkspaceType(string wspath)
+		{
+			if (File.Exists(wspath+@"\.pydevproject")) {
+				return WorkspaceType.Python;
+			} else if (File.Exists(wspath+@"\.classpath")) {
+				return WorkspaceType.Java;
+			} 
+			//TODO: sql workspace type here
+			
+			throw new InvalidProgramException("No workspace type identified."); 
+		}
+		
+		public static Workspace GetWorkspace(WorkspaceType type)
+		{
+			if (type == WorkspaceType.Java) {
+				return javaWorkspace;
+			} else if (type == WorkspaceType.Python) {
+				return pythonWorkspace;
+			} else if (type == WorkspaceType.SQL)  {
+				return sqlWorkspace;
+			}		
+			throw new InvalidProgramException("No desired workspace type.");
+		}
+		
+		public abstract string RawWSFolderName();
+		
+		/// <summary>
+		/// workspace init
+		/// </summary>
+		/// <returns>true - first time init, false - NOT first time</returns>
+		public abstract bool Init(string cdatPath, string wsPath);
+	}
+	
+	internal class JavaWorkspace : Workspace {
+		public override bool Init(string cdatPath, string wsPath) {
+			bool firstTimeInit = false;
+			//1.prepare workspace if required.
+			if (!(new DirectoryInfo(wsPath).Exists)){
+				//XTendLibs.FolderDelete.DeleteDirectory(workspace);
+				XTendLibs.FolderCopy.DirectoryCopy(cdatPath +@"\"+ RawWSFolderName(), wsPath, true, null, null);
+				firstTimeInit = true;
+			}
+			
+			//2.make config change if required.			
+			
+			return firstTimeInit;
+			
+		}
+		
+		public override string RawWSFolderName() {
+			return "rws.java";
+		}
+		
+	}
+	
+	internal class PythonWorkspace : Workspace {
+		
+		private const string ngo_placeholder = "___NGO_PYTHON_PLACHOLDER___";
+		private const string metaFileModulesKeys = @"\.metadata\.plugins\org.python.pydev\v1_ergtpsw9w30j9cfpojzkryfd\modulesKeys";
+		private const string metaFilePythonPath = @"\.metadata\.plugins\org.python.pydev\v1_ergtpsw9w30j9cfpojzkryfd\pythonpath";
+		
+		public override bool Init(string cdatPath,string wsPath) {
+			bool firstTimeInit = false;
+			//1.prepare workspace if required.
+			if (!(new DirectoryInfo(wsPath).Exists)){
+				//XTendLibs.FolderDelete.DeleteDirectory(workspace);
+				XTendLibs.FolderCopy.DirectoryCopy(cdatPath +@"\"+ RawWSFolderName(), wsPath, true, null, null);
+				firstTimeInit = true;
+			
+				//2.make config change for pydev if required.			
+				string textModuleKey = File.ReadAllText(wsPath+metaFileModulesKeys);
+				textModuleKey = textModuleKey.Replace(ngo_placeholder, CodeBase.GetCodePath());
+				File.WriteAllText(wsPath+metaFileModulesKeys, textModuleKey);
+				
+				string pythonPath = File.ReadAllText(wsPath+metaFilePythonPath);
+				pythonPath = pythonPath.Replace(ngo_placeholder, CodeBase.GetCodePath());
+				File.WriteAllText(wsPath+metaFilePythonPath, pythonPath);
+			}
+			
+			return firstTimeInit;
+		}
+		
+		public override string RawWSFolderName() {
+			return "rws.python";
+		}
+		
+	}
+	
+	internal class SQLWorkspace : Workspace {
+		public override bool Init(string cdatPath,string wsPath) {
+			throw new NotImplementedException();
+		}
+		
+		public override string RawWSFolderName() {
+			return "rws.sql";
+		}
+		
+	}
+}
