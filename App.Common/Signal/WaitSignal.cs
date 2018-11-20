73ca40e582f15d2111f2bb81ae3929858714df42
unknown <houxuyong@hotmail.com>
2018-11-20 11:37:19 +0800

    wait signal enhancement

--- App.Common/Signal/WaitSignal.cs
+++ App.Common/Signal/WaitSignal.cs
@@ -18,21 +18,52 @@ namespace App.Common.Signal
 	{
 		readonly ManualResetEvent signalInternal = new ManualResetEvent(false);
 		public Object AttechedObject {private set; get;}
+		public bool IsErrorOccured{private set; get;}
+		
+		private string errorString;
+		private string matchingString;
+		private bool isWaiting = false;
+		
 		
 		public WaitSignal()
 		{
+			IsErrorOccured = false;
 		}
+		
 		
 		public void PushObject(Object obj) {
 			this.AttechedObject = obj;
 		}
 		
 		public void WaitOne() {
+			if (isWaiting)
+				throw new InvalidProgramException("signal is in invalid status.");
+			this.isWaiting = true;
 			this.signalInternal.WaitOne();
+		}
+		
+		public WaitSignal WaitOneWhen(string mstr, string error) {
+			this.matchingString = mstr;
+			this.errorString = error;
+			WaitOne();
+			return this;
 		}
 		
 		public void Set() {
 			this.signalInternal.Set();
+			this.isWaiting = false;
+		}
+		
+		public void SetWhen(Object str) {
+			if (str == null || !this.isWaiting)
+				return;
+			if (str.ToString().Contains(this.matchingString)) {
+		    	Set();
+			} else if (str.ToString().Contains(this.errorString)) {
+				Set();
+				this.AttechedObject = str.ToString();
+				IsErrorOccured = true;
+			}
 		}
 		
 		public void Reset() {
