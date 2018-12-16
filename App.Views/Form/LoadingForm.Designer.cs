1cda92a602001fe3e46d4e3037ad86cebfd0e3f6
unknown <houxuyong@hotmail.com>
2018-12-16 22:05:45 +0800

    add loading progressbar form for course loading.

---
+++ App.Views/Form/LoadingForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Bob (XuYong Hou) houxuyong@hotmail.com
+ * Date: 2018/7/7
+ * Time: 11:40
+ * 
+ * 
+ */
+namespace App.Views
+{
+	partial class LoadingForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label lbText;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.lbText = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(0, 0);
+			this.progressBar1.Margin = new System.Windows.Forms.Padding(0);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(358, 23);
+			this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+			this.progressBar1.TabIndex = 0;
+			// 
+			// lbText
+			// 
+			this.lbText.BackColor = System.Drawing.Color.Transparent;
+			this.lbText.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.lbText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.lbText.Location = new System.Drawing.Point(360, 0);
+			this.lbText.Name = "lbText";
+			this.lbText.Size = new System.Drawing.Size(112, 23);
+			this.lbText.TabIndex = 1;
+			this.lbText.Text = "正在载入课程";
+			// 
+			// LoadingForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(484, 25);
+			this.Controls.Add(this.lbText);
+			this.Controls.Add(this.progressBar1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "LoadingForm";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ExitForm";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ App.Views/Form/LoadingForm.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Bob (XuYong Hou) houxuyong@hotmail.com
+ * Date: 2018/7/7
+ * Time: 11:40
+ * 
+ * 
+ */
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using App.Common.Controls;
+
+namespace App.Views
+{
+	/// <summary>
+	/// Description of ExitForm.
+	/// </summary>
+	public partial class LoadingForm : Form
+	{
+		//Delegate for cross thread call to close
+		private delegate void CloseDelegate();
+		private static LoadingForm loadingForm;
+		
+		LabelProgressBar progressbar;
+		
+		public LoadingForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			progressBar1.Height = this.Height;
+			//this.Width = progressBar1.Width;
+			timer1.Enabled = true;
+			timer1.Start();
+			timer1.Interval = 100;
+			progressBar1.Maximum = 100;
+			timer1.Tick += new EventHandler(timer1_Tick);
+		}
+		
+		
+		void timer1_Tick(object sender, EventArgs e)
+		{
+			if (progressBar1.Value != 100) {
+				progressBar1.Value++;
+			} else {
+				timer1.Stop();
+			}
+		}
+		
+		static public void ShowLoadingScreen()
+		{
+			// Make sure it is only launched once.
+			if (loadingForm != null)
+				return;
+			Thread thread = new Thread(new ThreadStart(LoadingForm.ShowForm));
+			thread.IsBackground = true;
+			thread.SetApartmentState(ApartmentState.STA);
+			thread.Start();           
+		}
+	
+		static private void ShowForm()
+		{
+			loadingForm = new LoadingForm();
+			Application.Run(loadingForm);
+		}
+	
+		static public void CloseForm()
+		{
+			loadingForm.Invoke(new CloseDelegate(LoadingForm.CloseFormInternal));
+		}
+	
+		static private void CloseFormInternal()
+		{
+			loadingForm.Close();
+		}
+	}
+}
